bf151b740160e16a942ee31b4ebe0311b7b7238f
Yohen Sheraun <yosherau@u81z04s02.42kl.edu.my>
2024-12-09 12:08:07 +0800

    With Bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yosherau <yosherau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/07 17:05:47 by ysheraun          #+#    #+#             */
+/*   Updated: 2024/12/09 12:04:08 by yosherau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# define NEWLINE '\n'
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 1024
+# endif
+
+void	*ft_memcpy(void *dst, void *src, size_t n);
+char	*ft_strchr(char *s, int c);
+char	*ft_strdup(char *s1);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(char *s);
+char	*get_next_line(int fd);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yosherau <yosherau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/07 17:05:28 by ysheraun          #+#    #+#             */
+/*   Updated: 2024/12/09 12:04:33 by yosherau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	*ft_memcpy(void *dst, void *src, size_t n)
+{
+	size_t	i;
+
+	if (!dst && !src)
+		return (dst);
+	i = -1;
+	while (++i < n)
+		((char *)dst)[i] = ((char *)src)[i];
+	return (dst);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	c = (unsigned char)c;
+	while (*s)
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == c)
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strdup(char *s1)
+{
+	int		i;
+	char	*dest;
+	size_t	length;
+
+	i = -1;
+	length = ft_strlen(s1);
+	dest = (char *)malloc((sizeof(char) * length) + 1);
+	if (!dest)
+		return (NULL);
+	while (s1[++i])
+		dest[i] = s1[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*ret;
+	size_t	len_s1;
+	size_t	len_s2;
+
+	if (!s1 && *s2)
+		return (ft_strdup(s2));
+	if (!s1 || !s2)
+		return (NULL);
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	ret = malloc(sizeof(char) * (len_s1 + len_s2) + 1);
+	if (!ret)
+	{
+		free(s1);
+		return (NULL);
+	}
+	ft_memcpy(ret, s1, len_s1);
+	ft_memcpy(ret + len_s1, s2, len_s2);
+	ret[len_s1 + len_s2] = '\0';
+	free(s1);
+	return (ret);
+}
+
+size_t	ft_strlen(char *s)
+{
+	int	count;
+
+	count = 0;
+	while (s[count])
+		count++;
+	return (count);
+}
